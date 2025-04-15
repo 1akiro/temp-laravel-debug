0b6397a03b523d0aabd9931ea014763d531cc7b9
Rocketman9631 <gbojarunecs@gmail.com>
2025-04-15 16:48:50 +0300

    added tailwindcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-<meta charset="UTF-8"/>
-<title>Hello World Application</title>
-</head>
-<body>
-<h1>Hello World!</h1>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </body>
 </html>
