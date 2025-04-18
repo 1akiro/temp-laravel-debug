6a6bc77de19183da0e84e23d276ff7d09df61bd5
Rocketman9631 <gbojarunecs@gmail.com>
2025-04-18 19:48:14 +0300

    updated styles

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,16 +1,16 @@
     <div class="mt-8">
-      <div class="flex bg-white h-96 container mx-auto">
+      <div class="flex bg-primary-white h-96 container mx-auto">
         <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
           <div>
             <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">
-                        <span class="text-blue-600">360&deg</span> Virtuālās Tūres
+                        <span class="text-accent-green">360&deg</span> Virtuālās Tūres
             </h2>
             <p class="mt-2 text-sm text-gray-500 md:text-base">
                         Mēs veidojam augstas kvalitātes 360° virtuālās tūres, kas ļauj iepazīt vidi attālināti – reālistiski un interaktīvi. Ideāli piemērots nekustamajiem īpašumiem, uzņēmumiem un publiskām telpām, kas vēlas izcelties digitālajā vidē.
             </p>
             <div class="flex justify-center lg:justify-start mt-6">
-            <button class="md:mt-0 mt-2 md:mr-0 mr-2 bg-blue-500 px-5 py-3 rounded-xl text-sm text-white hover:text-white shadow-xl hover:shadow-xl hover:shadow-blue-300/80 shadow-blue-400/40 hover:bg-blue-500-600">Izpētīt Katalogu</button>
-              <button class="md:mt-0 mt-2 ml-2 md:mr-0 mr-2 bg-blue-500 px-5 py-3 rounded-xl text-sm text-white hover:text-white shadow-xl hover:shadow-xl hover:shadow-blue-300/80 shadow-blue-400/40 hover:bg-blue-600">Sazinies ar mums</button>
+            <button class="md:mt-0 mt-2 md:mr-0 mr-2 bg-accent-green px-5 py-3 rounded-xl text-sm text-white hover:text-white shadow-xl hover:shadow-xl hover:shadow-blue-300/80 shadow-blue-400/40 hover:bg-blue-500-600">Izpētīt Katalogu</button>
+              <button class="md:mt-0 mt-2 ml-2 md:mr-0 mr-2 bg-accent-green px-5 py-3 rounded-xl text-sm text-white hover:text-white shadow-xl hover:shadow-xl hover:shadow-blue-300/80 shadow-blue-400/40 hover:bg-blue-600">Sazinies ar mums</button>
             </div>
           </div>
         </div>
