7c1c0ebb53eef6a4bdd31f2083fa4ab9c24808a2
Rocketman9631 <gbojarunecs@gmail.com>
2025-06-03 15:51:52 +0300

    added routes to tour.index

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="flex space-x-ma5 text-ma4 font-semibold text-dark">
             <a href="{{ route('home') }}">Sākumlapa</a>
-            <a href="#">Katalogs</a>
+            <a href="{{ route('tour.index')}}">Katalogs</a>
             <a href="#">Kontakti</a>
         </div>
         <div class="flex space-x-ma3">
@@ -15,9 +15,7 @@
         </div>
     </div>
 
-    <!-- Mobile Navbar -->
     <div class="lg:hidden">
-        <!-- Top row: Logo and burger -->
         <div class="flex items-center justify-between w-full">
             <div class="text-green-700 text-ma6 font-bold text-left">
                 Virtuālās tūres
@@ -27,10 +25,9 @@
             </button>
         </div>
 
-        <!-- Mobile navigation items -->
         <div id="mobileNav" class="hidden flex flex-col gap-ma4 mt-ma4 text-ma4 font-semibold text-dark items-start text-left">
             <a href="{{ route('home') }}">Sākumlapa</a>
-            <a href="#">Katalogs</a>
+            <a href="{{ route('tour.index')}}">Katalogs</a>
             <a href="#">Kontakti</a>
             <a class="hover:bg-green-700 hover:text-white text-dark font-bold py-2 px-4 border-2 border-green-700 hover:border-green-700 rounded-xl min-w-[10rem] text-left" href="{{ route('show.register') }}">Reģistrēties</a>
             <a class="hover:bg-green-700 hover:text-white text-dark font-bold py-2 px-4 border-2 border-green-700 hover:border-green-700 rounded-xl min-w-[10rem] text-left" href="{{ route('show.login') }}">Pieslēgties</a>
